7664889c8149010ae74408e37b028230e964fa06
Bud Byrd <bud.byrd@rackspace.com>
2013-02-20 09:24:11 -0600

    Initial project checkin.

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,27 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global")
+    log "warn"
+    repositories {
+        grailsCentral()
+        mavenCentral()
+    }
+    dependencies {
+        // We use the jaxrs plugin, which uses jersey 1.8.
+        // When the jaxrs plugin updates, we'll need to update this version
+        // so that the core library version matches.
+        compile 'com.sun.jersey:jersey-client:1.8'
+    }
+
+    plugins {
+        build(":tomcat:$grailsVersion",
+              ":release:2.0.3",
+              ":rest-client-builder:1.0.2") {
+            export = false
+        }
+    }
+}
