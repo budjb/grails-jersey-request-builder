494f8e3065a181becbbc937b32dba741a7123351
Burt Beckwith <burt@burtbeckwith.com>
2013-09-23 20:14:07 -0400

    dropped minimum Grails version to 2.0

--- RequestBuilderGrailsPlugin.groovy
+++ RequestBuilderGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class RequestBuilderGrailsPlugin {
     // the plugin version
     def version = "1.1.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.1 > *"
+    def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
