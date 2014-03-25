6082b17d94236f497770ff0dd7a76b63162acac0
Aaron Brown <brown.aaron.lloyd@gmail.com>
2014-03-24 19:20:31 -0500

    Client behavior through Config.
    Client Behavior defaults controlled by Config rather than hard-coded.
    
    Defaults are implemented in the DefaultJerseyRequestBuilderConfig.

---
+++ grails-app/conf/DefaultJerseyRequestBuilderConfig.groovy
@@ -0,0 +1,19 @@
+jerseyRequestBuilder {
+    connectionTimeout = null
+    readTimeout = null
+    contentType = null
+    accept = null
+    convertJson = true
+    convertXML = true
+    binaryResponse = false
+    skipStatusCheck = false
+    rawClientResponse = false
+    followRedirects = true
+    debug = false
+    ignoreInvalidSSL = false
+    useBasicAuth = false
+    basicAuthUserName = null
+    basicAuthPassword = null
+    chunkSize = null
+    encodeGzip = false
+}
