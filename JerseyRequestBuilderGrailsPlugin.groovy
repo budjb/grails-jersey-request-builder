6b959bf86e6aa5f1440064381695350e0d2373d3
Bud Byrd <bud.byrd@rackspace.com>
2014-04-11 07:56:19 -0500

    Release 1.2.1.

--- JerseyRequestBuilderGrailsPlugin.groovy
+++ JerseyRequestBuilderGrailsPlugin.groovy
@@ -21,7 +21,7 @@ import org.codehaus.groovy.grails.commons.GrailsApplication
  * limitations under the License.
  */
 class JerseyRequestBuilderGrailsPlugin {
-    def version = "1.2.0"
+    def version = "1.2.1"
     def grailsVersion = "2.0 > *"
     def title = "Jersey Request Builder Plugin"
     def author = "Bud Byrd"
